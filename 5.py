f858c09675f1a094ec2f1150687b6022ab58ddd3
kd699 <kd69931@gmail.com>
2026-01-27 12:49:05 +0200

    add gitignore

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 # ? составные выражения / (and, or)
 
-# * 1. Оператор and (И) Выражение истинно, только если ОБА условия верны
+# * Оператор and (И) Выражение истинно, только если ОБА условия верны
 age = 25
 balance = 1000
 if age >= 18 and balance > 500:
@@ -15,6 +15,15 @@ if day == "Saturday" or day == "Sunday":
 else:
     print("Сегодня рабочий день")
 
+
+# * Оператор not (НЕ) Инвертирует (переворачивает) результат условия
+is_banned = False
+if not is_banned:
+    print("Доступ разрешен. Вы не забанены.")
+else:
+    print("Доступ к ресурсу запрещен.")
+
+
 # ? вложенные конструкции / nested structures
 username = "admin"
 password = "1234"
@@ -26,3 +35,20 @@ if username == "admin":
         print("Неверный пароль.")
 else:
     print("Такого пользователя не существует.")
+
+
+# ! Важно помнить порядок выполнения (приоритет):
+# * not (выполняется первым)
+# * and (выполняется вторым)
+# * or (выполняется последним)
+# * Скобки () меняют этот порядок.
+
+
+# ? задачи
+x,y,z = True, False, True
+print(x or y and z)
+print(x and y or x)
+print(not x or y and z)
+print(not x and not y or z)
+print(x and x or y and x or x)
+print((x or y) and (not z))
