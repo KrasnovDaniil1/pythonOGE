ef94b3388b4492e743faa6e97d0e221b89e82230
kd699 <kd69931@gmail.com>
2026-02-26 21:49:48 +0200

    add gitignore

--- test.py
+++ test.py
@@ -5,33 +5,42 @@ tracer(False)
 
 m=20
 
+left(90)
 
 for i in range(2):
-    forward(10 * m)
+    forward(28 * m)
     right(90)
     forward(18 * m)
     right(90)
 
 penup()
 
-forward(5 * m)
+forward(14 * m)
 right(90)
-forward(7 * m)
+forward(10 * m)
 right(90)
 
 pendown()
 
 for i in range(2):
-    forward(10 * m)
+    forward(30 * m)
     right(90)
     forward(7 * m)
     right(90)
 
 penup()
-for x in range(0, 11):
-    for y in range(-18, 1):
+
+x_count = 0
+y_count = 0
+for x in range(0, 19):
+    x_count += 1
+    y_count = 0
+    for y in range(0, 29):
         goto(x*m, y*m)
         dot(3, "red")
+        y_count += 1
+
+print(x_count * y_count )
 
 update()
 done()
